c8eca0cb6421955e8560c911165bd24fdfa4677d
ASwiftguru <rajinikrai@gmail.com>
2019-06-20 09:56:24 +0200

    Persisiting data using Codable protocol and Plist

--- sillybilly/Controllers/TodoListViewController.swift
+++ sillybilly/Controllers/TodoListViewController.swift
@@ -11,27 +11,18 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard
-    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "first item"
-        itemArray.append(newItem)
+      
+        print (dataFilePath)
         
-        let newItem2 = Item()
-        newItem2.title = "2nd item"
-        itemArray.append(newItem2)
         
-        let newItem3 = Item()
-        newItem3.title = "3rd item"
-        itemArray.append(newItem3)
-        
-        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
-            itemArray = items
-        }
+      loadItems()
+
     }
     
     //MARK - Tableview DataSource methods
@@ -70,21 +61,19 @@ class TodoListViewController: UITableViewController {
     
     //MARK - Tableview delegate methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
         
-        print ("CellForRowAtIndexPath Called")
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        //itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+//Above line of code is same as below code
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        }
+//        else {
+//            itemArray[indexPath.row].done = false
+//        }
+//
+        saveItems()
         
-    
-        if itemArray[indexPath.row].done == false {
-            itemArray[indexPath.row].done = true
-        }
-        else {
-            itemArray[indexPath.row].done = false
-        }
-        
-       tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -104,10 +93,18 @@ class TodoListViewController: UITableViewController {
             newItem.title = textField.text!
             
             self.itemArray.append(newItem)
+
+            let encoder = PropertyListEncoder()
+            do {
+                let data = try encoder.encode(self.itemArray)
+                try data.write(to: self.dataFilePath!)
+            }
+            catch {
+                print ("Error encoding item array \(error)")
+            }
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
-            self.tableView.reloadData()
+            self.saveItems()
             
         }
         
@@ -122,6 +119,29 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    //MARK - (Save items) - Model manupulation methods
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }
+        catch {
+            print ("Error encoding item array \(error)")
+        }
+         self.tableView.reloadData()
+    }
     
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array \(error)")
+            }
+        
+        }
+    }
 }
 
